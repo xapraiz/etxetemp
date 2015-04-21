62e65ffd84e3be0e231b04322afefd80c4cd91bf
xapraiz <xabier_apraiz@hotmail.com>
2015-04-21 11:46:17 +0200

    Dropboxekin integrazioa garatzen

---
+++ js/xabi.js
@@ -0,0 +1,11 @@
+/**
+ * Created by xabier on 21/04/15.
+ */
+
+
+function klikTw(){
+    alert("Garatzen ari gara Twitterrekin partekatzeko aukera");
+}
+function klikDb(){
+    alert("Garatzen ari gara Dropboxen gordetzeko aukera");
+}
--- main.py
+++ main.py
@@ -41,6 +41,102 @@ class MainHandler(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('jinja_template.html')
         self.response.write(template.render(template_values))
 
+
+
+
+
+#############
+## DROPBOX ##
+#############
+
+class BaseHandler(webapp2.RequestHandler):
+
+    def dispatch(self):
+        # Get a session store for this request.
+        self.session_store = sessions.get_store(request=self.request)
+
+        try:
+            # Dispatch the request.
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            # Save all sessions.
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        # Returns a session using the default cookie key.
+        return self.session_store.get_session()
+
+config = {}
+config['webapp2_extras.sessions'] = {'secret_key': 'my-super-secret-key'}
+
+
+
+
+dropbox_app_key='kq1dnywotn5al03'
+gae_callback_url='https://etxetemp.appspot.com/oauth_callbackDropbox'
+dropbox_app_secret=''
+
+class LoginAndAuthorizeDropbox(webapp2.RequestHandler):
+    def get(self):
+
+        url='https://www.dropbox.com/1/oauth2/authorize'
+        parametroak= {'response_type':'code',
+                      'client_id':dropbox_app_key,
+                      'redirect_uri':gae_callback_url
+        }
+        parametroak=urllib.urlencode(parametroak)
+        self.redirect(url + '?' + parametroak)
+
+
+
+class OAuthCallbackDropbox(BaseHandler):
+    def get(self):
+        request_url= self.request.url
+        code=request_url.split('code=')[1]
+
+        http=httplib2.Http()
+        metodoa='POST'
+        url='https://api.dropbox.com/1/oatuh2/token'
+        parametroak={'code':code,
+                     'grant_type': 'authorization.code',
+                     'client_id': dropbox_app_key,
+                     'client_secret':dropbox_app_secret,
+                     'redirect_uri':gae_callback_url}
+
+        parametroak=urllib.urlencode(parametroak)
+        erantzuna, edukia=http.request(url, metodoa,body=parametroak, headers={})
+
+        #self.response.write(edukia)
+
+        json_edukia= json.load(edukia)
+        self.session['access_token']=json_edukia['access_token']
+        self.redirect('/welcomePage')
+
+
+
+
+class WelcomePageDropbox(BaseHandler):
+    def get(self):
+        access_token=self.session['access_token']
+
+        http=httplib2.Http()
+        method='PUT'
+        path='/karpeta/fitxategia.txt'
+        url='https://api-content.dropbox.com/1/files_put/auto' + path
+        parametroak={'overwrite':'false'}
+        parametroak=urllib.urlencode(parametroak)
+        goiburuak={}
+        goiburuak['Authorization']='Bearer ' + access_token
+        edukia='Hau fitxategiaren eduki da,,, probaa'
+        resp, content = http.request(url + '?' + parametroak, method, body=edukia, headers=goiburuak)
+
+        self.response.write('Egiaztatu zure Dropbox kontuan fitxategia sortu dela')
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/LoginAndAuthorizeDropbox', LoginAndAuthorizeDropbox),
+    ('/oauth_callbackDropbox', OAuthCallbackDropbox),
+    ('/welcomePageDropbox', WelcomePageDropbox)
 ], debug=True)
